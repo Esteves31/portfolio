09fe166837a1c208ad6da603bbf27784f95c17d3
Matheus <matheus.resteves@gmail.com>
2026-08-04 14:39:41 -0300

    ::dizzy: adicionando os components a tela

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import Navbar from "./components/Navbar.jsx";
 import FirstSection from "./components/FirstSection.jsx";
+import AboutMeSection from "./components/AboutMeSection.jsx";
+import ExperienceSection from "./components/ExperienceSection.jsx";
+import MySkillsSection from "./components/MySkillsSection.jsx";
+import MyProjects from "./components/MyProjects.jsx";
+import Blog from "./components/Blog.jsx";
+import ContactSection from "./components/ContactSection.jsx";
 import Footer from "./components/Footer.jsx";
 import "./App.css";
 
@@ -8,6 +14,11 @@ function App() {
     <>
       <Navbar />
       <FirstSection />
+      <AboutMeSection />
+      <ExperienceSection />
+      <MySkillsSection />
+      <MyProjects />
+      <ContactSection />
       <Footer />
     </>
   );
